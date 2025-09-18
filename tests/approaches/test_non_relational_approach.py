9202afb629fc7d0ae38b464a87f3770e1c58b04a
Tom Silver <tomssilver@gmail.com>
2025-09-18 10:21:53 -0400

    add make video script

--- tests/approaches/test_non_relational_approach.py
+++ tests/approaches/test_non_relational_approach.py
@@ -8,6 +8,8 @@ from prbench_bilevel_planning.env_models import create_bilevel_planning_models
 from alphatamp.approaches.non_relational_approach import (
     NonRelationalApproach,
 )
+from conftest import MAKE_VIDEOS
+from gymnasium.wrappers import RecordVideo
 
 
 def test_non_relational_approach():
@@ -15,7 +17,9 @@ def test_non_relational_approach():
 
     # Test in a PRBench environment where there are multiple objects to move around.
     prbench.register_all_environments()
-    env = prbench.make("prbench/ClutteredRetrieval2D-o10-v0")
+    env = prbench.make("prbench/ClutteredRetrieval2D-o10-v0", render_mode="rgb_array")
+    if MAKE_VIDEOS:
+        env = RecordVideo(env, "unit_test_videos")
     env_models = create_bilevel_planning_models(
         "clutteredretrieval2d",
         env.observation_space,
@@ -35,6 +39,7 @@ def test_non_relational_approach():
     # Evaluation should take a long time due to bad heuristic.
     start_planning_time = time.time()
     plan = approach.run_planning(obs, timeout=100)
+    import ipdb; ipdb.set_trace()
 
     for action in plan.actions:
         _, _, done, _, _ = env.step(action)
