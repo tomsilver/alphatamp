e8b2b572823faee285e6468d374b0b3acfdac98a
SkywalkerJLi <126359826+SkywalkerJLi@users.noreply.github.com>
2025-09-23 13:21:30 -0400

    Create new scoring utils folder (#7)
    * add conftest.py
    
    * fix linting
    
    * put BatchRankingAbstractPlanGenerator in separate folder

---
+++ src/alphatamp/scoring_utils/batch_ranking.py
@@ -0,0 +1,78 @@
+"""Generates batches of abstract plans and then ranks them using a score function, where
+higher scores are considered better."""
+
+from itertools import islice
+from typing import Callable, Iterator, TypeAlias, TypeVar
+
+from bilevel_planning.abstract_plan_generators.abstract_plan_generator import (
+    AbstractPlanGenerator,
+)
+from bilevel_planning.bilevel_planning_graph import BilevelPlanningGraph
+from bilevel_planning.structs import (
+    Goal,
+    RelationalAbstractState,
+)
+from relational_structs import GroundOperator
+
+_X = TypeVar("_X")  # state
+_U = TypeVar("_U")  # action
+Skeleton: TypeAlias = tuple[list[RelationalAbstractState], list[GroundOperator]]
+
+
+class BatchRankingAbstractPlanGenerator(
+    AbstractPlanGenerator[_X, RelationalAbstractState, GroundOperator]
+):
+    """Generates batches of abstract plans and then ranks them using a score function,
+    where higher scores are considered better."""
+
+    def __init__(
+        self,
+        base_generator: AbstractPlanGenerator[
+            _X, RelationalAbstractState, GroundOperator
+        ],
+        score_fn: Callable[[Skeleton, list[Skeleton]], float],
+        batch_size: int,
+        seed: int,
+    ) -> None:
+        self._base_generator = base_generator
+        self._score_fn = score_fn
+        self._batch_size = batch_size
+        # In the future, make this public or find another workaround.
+        abstract_successor_fn = (
+            self._base_generator._abstract_successor_function  # pylint: disable=protected-access
+        )
+        super().__init__(abstract_successor_fn, seed)
+
+    def __call__(
+        self,
+        x0: _X,
+        s0: RelationalAbstractState,
+        goal: Goal,
+        timeout: float,
+        bpg: BilevelPlanningGraph[_X, _U, RelationalAbstractState, GroundOperator],
+    ) -> Iterator[Skeleton]:
+
+        # This should be refactored soon.
+        iterator = self._base_generator(x0, s0, goal, timeout, bpg)
+        prev: list[Skeleton] = []
+        while batch := list(islice(iterator, self._batch_size)):
+            # NOTE: we need to reorder after every failed attempt because of prev.
+            while batch:
+                tiebreaking_score_fn = lambda x: (
+                    self._score_fn(x, prev),
+                    -len(x[1]),
+                    self._rng.uniform(),
+                )
+                batch.sort(key=tiebreaking_score_fn)
+                skeleton = batch.pop()
+
+                # Uncomment to debug.
+                # print("YIELDING")
+                # for a in skeleton[1]:
+                #     print(a.short_str)
+                # print()
+
+                yield skeleton
+
+                # NOTE: assuming that every previous skeleton failed.
+                prev.append(skeleton)
---
+++ tests/scoring_utils/test_batch_ranking.py
@@ -0,0 +1,84 @@
+"""Tests for batch_ranking.py."""
+
+from typing import TypeAlias
+
+import prbench
+from bilevel_planning.abstract_plan_generators.abstract_plan_generator import (
+    AbstractPlanGenerator,
+)
+from bilevel_planning.abstract_plan_generators.heuristic_search_plan_generator import (
+    RelationalHeuristicSearchAbstractPlanGenerator,
+)
+from bilevel_planning.structs import (
+    RelationalAbstractState,
+)
+from bilevel_planning.trajectory_samplers.parameterized_controller_sampler import (
+    ParameterizedControllerTrajectorySampler,
+)
+from bilevel_planning.utils import (
+    RelationalControllerGenerator,
+)
+from prbench_bilevel_planning.env_models import create_bilevel_planning_models
+from relational_structs import GroundOperator
+
+from alphatamp.scoring_utils.batch_ranking import BatchRankingAbstractPlanGenerator
+
+Skeleton: TypeAlias = tuple[list[RelationalAbstractState], list[GroundOperator]]
+
+
+def _score_skeleton(skeleton: Skeleton, _: list[Skeleton]) -> float:
+    """Naive scoring function: longer length is better"""
+
+    return len(skeleton[0])
+
+
+def test_batch_ranking():
+    """Tests for BatchRankingAbstractPlanGenerator()."""
+
+    # Test in a PRBench environment where the first skeleton won't work.
+    prbench.register_all_environments()
+    env = prbench.make("prbench/Obstruction2D-o1-v0")
+    env_models = create_bilevel_planning_models(
+        "obstruction2d", env.observation_space, env.action_space, num_obstructions=1
+    )
+
+    # Set parameter values
+    max_skill_horizon = 100
+    heuristic_name = "hff"
+    skeleton_batch_size = 100
+    seed = 123
+
+    ## Create the planning components.
+
+    # Create the sampler.
+    trajectory_sampler = ParameterizedControllerTrajectorySampler(
+        controller_generator=RelationalControllerGenerator(env_models.skills),
+        transition_function=env_models.transition_fn,
+        state_abstractor=env_models.state_abstractor,
+        max_trajectory_steps=max_skill_horizon,
+    )
+
+    assert isinstance(trajectory_sampler, ParameterizedControllerTrajectorySampler)
+
+    # Create the abstract plan generator.
+    base_abstract_plan_generator: AbstractPlanGenerator = (
+        RelationalHeuristicSearchAbstractPlanGenerator(
+            env_models.types,
+            env_models.predicates,
+            env_models.operators,
+            heuristic_name,
+            seed=seed,
+        )
+    )
+    batched_abstract_plan_generator: AbstractPlanGenerator = (
+        BatchRankingAbstractPlanGenerator(
+            base_abstract_plan_generator,
+            score_fn=_score_skeleton,
+            batch_size=skeleton_batch_size,
+            seed=seed,
+        )
+    )
+
+    assert isinstance(batched_abstract_plan_generator, AbstractPlanGenerator)
+
+    env.close()
