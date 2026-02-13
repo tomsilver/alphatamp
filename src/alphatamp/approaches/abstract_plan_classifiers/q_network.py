0117045daeb0b5c3f185a5bc00e2fc9097d68a09
SkywalkerJLi <sl5183@princeton.edu>
2026-02-12 22:07:38 -0500

    create test for QFunction training

--- src/alphatamp/approaches/abstract_plan_classifiers/q_network.py
+++ src/alphatamp/approaches/abstract_plan_classifiers/q_network.py
@@ -73,9 +73,9 @@ def create_abstract_plan_sequence(
                     create_abstract_state_embedding(all_ground_atoms, states[0]),
                     np.zeros(len(all_ground_operators)),
                 ]
-            )
+            ).reshape(1, -1).astype(np.float32)
             return (state_action_embedding, 1)
-        return np.array([[0.0, 0.0]], dtype=np.float32), 1
+        return np.zeros((1, len(all_ground_atoms) + len(all_ground_operators)), dtype=np.float32), 1
 
     sequence = []
     for i in range(seq_len):
