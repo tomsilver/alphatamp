89b9092eaedabf9150b977c88c20fc26960b8f11
Richard Zhou <rz3911@princeton.edu>
2026-04-01 16:15:08 -0400

    tests

--- tests/approaches/test_reprompt_approach.py
+++ tests/approaches/test_reprompt_approach.py
@@ -1,43 +1,43 @@
 """Tests for oracle_skeleton_generator_approach.py."""
 
 import imageio.v2 as iio
-import prbench
+import kinder
 import pytest
 from conftest import MAKE_VIDEOS
 from gymnasium.wrappers import RecordVideo
-from prbench_bilevel_planning.env_models import create_bilevel_planning_models
+from kinder_bilevel_planning.env_models import create_bilevel_planning_models
 
 from alphatamp.approaches.cluttered_storage.reprompt_approach import (
     RepromptApproach,
 )
 
 
-@pytest.mark.skip(reason="Requires LLM calls - run manually when needed")
+#@pytest.mark.skip(reason="Requires LLM calls - run manually when needed")
 def test_reprompt_approach():
     """Tests for RepromptApproach()."""
 
-    # Test in a PRBench environment
-    prbench.register_all_environments()
-    env = prbench.make("prbench/ClutteredStorage2D-b3-v0", render_mode="rgb_array")
+    # Test in a kinder environment
+    kinder.register_all_environments()
+    env = kinder.make("kinder/Obstruction2D-o3-v0", render_mode="rgb_array")
 
-    if MAKE_VIDEOS:
+    if True:
         env = RecordVideo(env, "unit_test_videos")
 
     # 2) Create bilevel models for this domain
     env_models = create_bilevel_planning_models(
-        "clutteredstorage2d",
+        "obstruction2d",
         env.observation_space,
         env.action_space,
-        num_blocks=3,
+        num_obstructions = 3
     )
 
     # Create the approach.
     approach = RepromptApproach(
-        env_models, seed=121, samples_per_step=10, training_planning_timeout=10
+        env_models, seed=122, samples_per_step=10, training_planning_timeout=10
     )
 
     # Train the approach
-    obs, _ = env.reset(seed=121)
+    obs, _ = env.reset(seed=122)
 
     img = env.render()
     iio.imsave("debug.png", img)
@@ -45,7 +45,7 @@ def test_reprompt_approach():
     approach.train(obs)  # no-op, but keeps the pattern consistent
 
     # Create a plan
-    plan = approach.run_planning(obs, timeout=300)
+    plan = approach.run_planning(obs, timeout=400)
 
     # Execute the plan
     for action in plan.actions:
